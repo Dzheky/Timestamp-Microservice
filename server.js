279f0d839b28d62a3bfa1333642e377227aecf2f
Evgeny Klimenchenko <zklimenchenko@gmail.com>
2016-07-11 08:06:28 +0000

    server change

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<h1>Hello World!</h1>');
 });
 
 app.listen(8080, function () {
